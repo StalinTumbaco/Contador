a0b4059ace569c2ac258c37be1c4c7a5f3b0f09e
StalinTumbaco <stalin2005tumbaco@gmail.com>
2025-07-02 23:53:19 -0500

    .1

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,8 +1,8 @@
 //Crea aqui tu componente Contador
-import { useState } from 'react';
+import React, { useState } from 'react';
 
-function Contador({ titulo, inicio }) {
-  const [contador, setContador] = useState(inicio);
+const Contador = () => {
+  const [contador, setContador] = useState(0);
 
   const incrementar = () => {
     setContador(contador + 1);
@@ -12,14 +12,18 @@ function Contador({ titulo, inicio }) {
     setContador(contador - 1);
   };
 
+  const reiniciar = () => {
+    setContador(0);
+  };
+
   return (
-    <div style={{ marginBottom: '20px' }}>
-        <h2>{titulo}</h2>
-        <p>Valor actual: {contador}</p>
-        <button onClick={incrementar}>+1</button>
-        <button onClick={decrementar}>-1</button>
+    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px', marginBottom: '20px'}}>
+      <h2>Contador: {contador}</h2>
+      <button onClick={incrementar}>+1</button>
+      <button onClick={decrementar}>-1</button>
+      <button onClick={reiniciar}>Reiniciar</button>
     </div>
   );
-}
+};
 
 export default Contador;
